31fd43988a4b6114b0c48acf365d1a0c2142bdaa
stevexnx <stevexnx@gmail.com>
2018-06-08 16:57:42 +0200

    funcionales

---
+++ mas_el_anterior.py
@@ -0,0 +1,7 @@
+while intento <= 3:
+    intento = 0
+    print("determinar mayores")
+    numero1 = input("escriba un numero: ")
+    numero2 = input("escriba un numero mayor que el anterior: ")
+    if numero1 >= numero2:
+        print(%d "no es mayor a "%d %numero2 %nuemero1 )
