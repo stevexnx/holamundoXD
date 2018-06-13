9c00b68df7e7756a8be61d47574ac76441b30403
stevexnx <stevexnx@gmail.com>
2018-06-14 01:04:30 +0200

    siempre da 101 XD

--- pulgadas.py
+++ pulgadas.py
@@ -3,6 +3,9 @@ while 1 == 1:
     yarda = pulgadas * 0.0277778
     pie = pulgadas * 0.0833333
     centimetro = pulgadas * 2.54
+    yarda = float(yarda)
+    pie = float(pie)
+    centimetro = float(centimetro)
     print("EN YARDAS ES IGUAL A:")
     print(yarda)
     print("EN PIES ES IGUAL A:")
---
+++ pulgadas2.py
@@ -0,0 +1,4 @@
+pulgadas = int(input("cantidad de pulgadas: "))
+yardas = pulgadas - 36
+if yardas <= 0:
+    
