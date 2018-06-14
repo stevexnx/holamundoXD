29a8fd2b66e5d1f0a95b938a41a310d9d4f0ab4a
stevexnx <stevexnx@gmail.com>
2018-06-14 18:20:32 +0200

    casi funciona

--- pares.py
+++ pares.py
@@ -1,23 +1,20 @@
-n1 = int(input("introducir primer numero: "))
-n2 = int(input("introducir segundo numero: "))
-n3 = int(input("introducir tercer numero: "))
-n4 = int(input("introducir cuarto numero: "))
-if n1 % 2 == 0:
-    print (n1,"es par")
-else:
-    print(n1,"no es par")
-
-if n2 % 2 == 0:
-        print (n2,"es par")
-else:
-    print(n2,"no es par")
-
-if n3 % 2 == 0:
-    print (n3,"es par")
-else:
-    print(n3,"no es par")
-
-if n4 % 2 == 0:
-    print (n4,"es par")
-else:
-    print(n4,"no es par")
+print("determinar pares")
+contador3 = 0
+contador = 0
+contador1 = 0
+veces = int(input("veces: "))
+while 1 == 1:
+    if veces != int(veces):
+        print("fin del programa")
+        break
+    while contador3 < veces:
+        contador3 += 1
+        numero = int(input("introducir numero: "))
+        if numero % 2 == 0:
+            contador += 1
+        else:
+            contador1 += 1
+        print(contador,"son pares")
+        print(contador1,"son impares")
+        if contador3 == veces:
+            print("fin del programa XD")    
