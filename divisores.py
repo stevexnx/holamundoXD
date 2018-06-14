861e78a775c0dc61f232870d63626475c3b95a9c
stevexnx <stevexnx@gmail.com>
2018-06-14 19:33:37 +0200

    sip

---
+++ divisores.py
@@ -0,0 +1,8 @@
+print("determinar divisores: ")
+numero = int(input("ya sabes que hacer :v "))
+n1 = numero
+while numero % n1 == 0:
+       print(n1)
+while n1 > 0:
+    n1 -= 1            
+   
--- mas_el_anterior.py
+++ mas_el_anterior.py
@@ -1,11 +1,14 @@
-intento = 0
-while intento <= 3:
+while 1 == 1:
     print("determinar mayores")
     numero1 = input("escriba un numero: ")
-    numero2 = input("escriba un numero mayor que el anterior: ")
-    if numero1 >= numero2:
-        print(numero2 +" no es mayor a " + numero1 )
-    elif intento + 1:
-        if numero1 < numero2:
-            print(numero1 + " fue el menor y el mayor fue " + numero2 )
-            print("programa terminado")     
+    numero2 = input("escriba un numero mayor a "+numero1 + ": ")
+    if numero1 < numero2:
+        print(numero1," fue el menor y el mayor fue ",numero2 )
+        print("programa terminado")
+    while numero2 < numero1:     
+        print(numero2," no es mayor a ",numero1)
+        numero2 = input("intente de nuevo: ")
+        if numero2 > numero1:
+            print(numero1," fue el menor y el mayor fue ",numero2 )
+            print("programa terminado")
+        
