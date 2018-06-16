544cee284883b5cf6f72b6f6f5f9c6154f68f60f
stevexnx <stevexnx@gmail.com>
2018-06-16 16:44:18 +0200

    la 30 es la vencida

--- pulgadas2.py
+++ pulgadas2.py
@@ -1,4 +1,15 @@
 pulgadas = int(input("cantidad de pulgadas: "))
-yardas = pulgadas - 36
-if yardas <= 0:
-    
+contadorY = 0
+contadorP = 0
+while 1 == 1:
+    if pulgadas >= 36:
+        contadorY += 1
+        pulgadas -= 36
+    elif pulgadas >= 12:
+        contadorP += 1
+        pulgadas -= 12
+    else:
+        print(pulgadas, "pulgadas es igual a",contadorY, "yardas")    
+        print(contadorP,"pies")
+        print("y sobran",pulgadas,"pulgadas")
+        break
