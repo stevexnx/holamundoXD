56b7e411fb449d9e42ec4a5e87b3f062c2d2c270
stevexnx <stevexnx@gmail.com>
2018-06-19 17:00:41 +0200

    tamaño de palabra?

--- divisores.py
+++ divisores.py
@@ -1,8 +1,9 @@
-print("determinar divisores: ")
-numero = int(input("ya sabes que hacer :v "))
-n1 = numero
-while numero % n1 == 0:
-       print(n1)
-while n1 > 0:
-    n1 -= 1            
-   
+
+numero = int(input("numero: "))
+for i in range(1,numero):
+     if numero % i == 0:
+        print(i)
+if numero < 0:
+          print("solo numeros positivos")
+if numero == 0:
+          print("cero no se puede dividir :v")
